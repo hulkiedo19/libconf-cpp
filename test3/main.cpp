3eb0417776852a9a5d5110016c9a6abd56b94aba
hulkiedo19 <artemmironovhaker@gmail.com>
2022-04-23 20:50:54 +0300

    minor changes

--- test3/main.cpp
+++ test3/main.cpp
@@ -1,127 +1,23 @@
 #include <iostream>
 #include <fstream>
-#include <string>
-
 #include "config_io.h"
-
-/*class config_io {
-	std::string filename;
-	std::string line;
-
-	std::ifstream input_file;
-	std::ofstream output_file;
-public:
-	config_io(std::string filename) {
-		this->filename = filename;
-	}
-
-	void AppendLine(std::string data) {
-		output_file.open(filename, std::ios::app);
-
-		if (output_file.is_open()) {
-			output_file << data << std::endl;
-		}
-
-		output_file.close();
-	}
-
-	std::string ReadLine() {
-		bool is_clear_flag = false;
-		static int current_line = 0;
-
-		input_file.open(filename);
-
-		if (input_file.is_open()) {
-			int cur_line = 0;
-
-			do {
-				if (!std::getline(input_file, line)) {
-					std::cout << std::endl << "counter is cleared to 0" << std::endl;
-					current_line = 0;
-					is_clear_flag = true;
-					break;
-				}
-			} while (cur_line++ < current_line);
-		}
-
-		input_file.close();
-
-		if(!is_clear_flag) current_line++;
-		
-		return line;
-	}
-
-	void ReadFile() {
-		input_file.open(filename);
-
-		if (input_file.is_open()) {
-			std::cout << "file: " << filename << std::endl;
-			while (std::getline(input_file, line)) {
-				std::cout << line << std::endl;
-			}
-		}
-
-		input_file.close();
-	}
-
-	std::string ReadFileToString() {
-		std::string buffer = "";
-
-		input_file.open(filename);
-
-		if (input_file.is_open()) {
-			std::cout << "file: " << filename << std::endl;
-			while (std::getline(input_file, line)) {
-				buffer += (line + "\n");
-			}
-		}
-
-		input_file.close();
-
-		return buffer;
-	}
-
-
-};*/
 
 int main() {
 	std::string filename = "D:\\text.txt";
 
 	config_io config(filename);
 
-	/*config.ReadFile();
+	/*
+	config.ReadFile();
 	config.AppendLine("hello_world-1");
 
 	std::cout << std::endl;
-	config.ReadFile();*/
-
-	/*std::cout << config.ReadLine() << std::endl;
-	std::cout << config.ReadLine() << std::endl;
-	std::cout << config.ReadLine() << std::endl;
-	std::cout << config.ReadLine() << std::endl;
-	std::cout << config.ReadLine() << std::endl;
-	std::cout << config.ReadLine() << std::endl;
-	std::cout << config.ReadLine() << std::endl;
-	std::cout << config.ReadLine() << std::endl;
-	std::cout << config.ReadLine() << std::endl;
-	std::cout << config.ReadLine() << std::endl;
-	std::cout << config.ReadLine() << std::endl;
-	std::cout << config.ReadLine() << std::endl;
-	std::cout << config.ReadLine() << std::endl;
-	std::cout << config.ReadLine() << std::endl;
-	std::cout << config.ReadLine() << std::endl;
-	std::cout << config.ReadLine() << std::endl;
-	std::cout << config.ReadLine() << std::endl;
-	std::cout << config.ReadLine() << std::endl;
+	config.ReadFile();
 
 	std::cout << config.ReadLine() << std::endl;
-	std::cout << config.ReadLine() << std::endl;
-	std::cout << config.ReadLine() << std::endl;*/
+	*/
 
 	std::string fileData = config.ReadFileToString();
-
-	std::cout << std::endl;
-	std::cout << "file length: " << fileData.length() << std::endl;
 	std::cout << "file data:" << std::endl << fileData << std::endl;
 
 	return 0;
