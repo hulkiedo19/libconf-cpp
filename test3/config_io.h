b6d8e9479941c3603a7f37de5d829c870df46110
hulkiedo19 <artemmironovhaker@gmail.com>
2022-04-24 22:06:04 +0300

    minor changes

--- test3/config_io.h
+++ test3/config_io.h
@@ -10,25 +10,21 @@ namespace libconf {
 		config_io(std::string filename);
 		~config_io();
 
-		// buffer
 		std::string get_buffer();
 		void show_buffer();
 		bool fill_buffer(std::string filename);
 		bool dump_buffer();
 		bool dump_buffer(std::string filename);
 
-		// values
 		bool append_value(std::string name, std::string value);
 		bool delete_value(std::string name);
 		bool replace_value(std::string name, std::string new_value);
 		bool value_exists(std::string name);
 		std::string get_value(std::string name);
 
-		// split
 		std::vector<std::string> get_split_value(std::string name);
 		void show_split_value(std::string name);
 
-		// lines
 		bool append_line(std::string data);
 		bool delete_line(std::string line);
 		bool delete_line(int line_number);
